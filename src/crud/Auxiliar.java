0284b8c080f9116c28be3019068692c281306021
Claudio Filho <claudio.filho7@hotmail.com>
2023-03-29 19:57:49 -0300

    Meu primeio crud

---
+++ src/crud/Auxiliar.java
@@ -0,0 +1,36 @@
+package crud;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Auxiliar {
+
+	static TelaInicial menu = new TelaInicial();
+	static Scanner scn = new Scanner(System.in);
+
+	public final static void clearConsole() {
+		for (int i = 0; i < 100; ++i)
+			System.out.println();
+	}
+
+	public final static void voltaMenu() throws SQLException {
+
+		TelaInicial menu = new TelaInicial();
+
+		System.out.println("Deseja voltar ao Menu Inicial ? S-sim)");
+		String voltar = scn.nextLine();
+
+		if (voltar.equalsIgnoreCase("S")) {
+			Auxiliar.clearConsole();
+			TelaInicial.main(null);
+		} else {
+			System.out.println("Para Voltar ao Menu Inicial digite S");
+			voltar = scn.nextLine();
+			Auxiliar.clearConsole();
+			TelaInicial.main(null);
+
+		}
+
+	}
+
+}
